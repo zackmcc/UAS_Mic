6e363b0a7105f446f3ce71ac0cfec1a9fc6b780f
wolfer55 <wolfer.4@wright.edu>
2018-04-07 10:08:15 -0700

    Ive added the low pass filter in the process button function.  Now what needs to be done is testing it on a file we created.  THere is also a way to do a high pass filter as well as a way to add gain.

--- team-projects/main.py
+++ team-projects/main.py
@@ -140,8 +140,8 @@ class Application:
 			time = np.arange(0, float(audData.shape[0]), 1) / rate
 
 			#plot amplitude (or loudness) over time
-			plt.figure(1)
-			plt.subplot(211)
+			plt.figure(2)
+			plt.subplot(311)
 			plt.title('Graph of ' + self.fileName)
 			plt.xlabel('Time (Seconds');
 			plt.ylabel('Amplitude')
@@ -149,22 +149,61 @@ class Application:
 			
 
 			fourier=np.fft.fft(channel1)
-			plt.figure(1)
-			plt.subplot(212)
+			plt.figure(2)
+			plt.subplot(312)
 			plt.plot(fourier)
 			
 			plt.ylabel('Amplitude')
 			plt.xlabel('k')
-			plt.show()
+
+
+			plt.figure(3)
+			plt.title("Unfiltered")
+			plt.subplot(411)
+			Pxx, freqs, bins, im = plt.specgram(channel1, Fs=rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+			cbar=plt.colorbar(im)
+			plt.xlabel('Time (s)')
+			plt.ylabel('Frequency (Hz)')
+			cbar.set_label('Intensity dB')
+
+
+
+
+
+
+			## Pydub does not
+			song = AudioSegment.from_wav(self.fileName)
+			print("Entered Low Pass Filter")
+			newSong = song.low_pass_filter(3000)
+			newSong = newSong.low_pass_filter(3000)
+			newSong = newSong.low_pass_filter(3000)
+			newSong = newSong.low_pass_filter(3000)
+			newSong = newSong.low_pass_filter(5000)
+			newSong = newSong.low_pass_filter(5000)
+			newSong = newSong.apply_gain(+10.0)
+			print("Exited Low Pass Filter")
+			newSong.export(self.fileName + "Filtered" + ".wav",format="wav")
+			self.fileName = self.fileName+"Filtered.wav";
+		
+
+			createGraphs(self.fileName,"Filtered");
+			
+
+
+
+
+
+
 
 
 
 			#code that converts to a mp3 for playback
 			newFile = self.fileName.replace(".wav",".mp3")
-			AudioSegment.from_wav(self.fileName).export(newFile,format="mp3")
+			#AudioSegment.from_wav(self.fileName).export(newFile,format="mp3")
 			self.fileName = newFile
+			newSong.export(self.fileName,format="mp3")
 			self.loadFile()				
-
+			plt.show()
 			
 
 
@@ -181,6 +220,54 @@ class Application:
 			self.lastskip = 0;
 			#except:
 				#showerror("Cant open this type of File")
+
+
+def createGraphs(filename_to_graph,title):
+	print("Made it into createGraphs")
+	rate,audData=scipy.io.wavfile.read(filename_to_graph)
+
+	print(rate)
+	print(audData)
+
+	wav_length = audData.shape[0]/rate #rate in second
+	data_type = audData.dtype #data type
+	print(wav_length)
+	print(data_type)
+
+
+	channel1=audData[:,0]
+	#create a time variable in seconds
+	time = np.arange(0, float(audData.shape[0]), 1) / rate
+
+	#plot amplitude (or loudness) over time
+	plt.figure(1)
+	plt.subplot(211)
+	plt.title('Graph of ' + filename_to_graph)
+	plt.xlabel('Time (Seconds');
+	plt.ylabel('Amplitude')
+	plt.plot(time, channel1, linewidth=0.02, alpha=0.7)
+
+
+	fourier=np.fft.fft(channel1)
+	plt.figure(1)
+	plt.subplot(212)
+	plt.plot(fourier)
+	
+	plt.ylabel('Amplitude')
+	plt.xlabel('k')
+
+	
+	plt.figure(3)
+	plt.subplot(412)
+	Pxx, freqs, bins, im = plt.specgram(channel1, Fs=rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+	cbar=plt.colorbar(im)
+	plt.title("Filtered")
+	plt.xlabel('Time (s)')
+	plt.ylabel('Frequency (Hz)')
+	cbar.set_label('Intensity dB')
+
+	
+
 
 if __name__ == '__main__':
 	root = tk.Tk()
